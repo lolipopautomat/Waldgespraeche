8e9a3f6f62ab10061c57e62021de978aa14d3b02
lolipopautomat <nukenja@gmail.com>
2022-04-14 23:36:17 +0200

    Initialisierung

---
+++ HTML/script.js
@@ -0,0 +1,43 @@
+const toggleButton = document.getElementsByClassName("toggle")[0];
+const navbarLinks = document.getElementsByClassName("Navbar-links")[0];
+const Brandname = document.getElementsByClassName("Brandname")[0];
+const logo = document.getElementsByClassName("logo_home")[0];
+const toggle = document.getElementsByClassName("toggle")[0];
+
+const nav = document.querySelector("nav");
+const sectionOne = document.getElementsByClassName("home")[0];
+
+const sectionOneOptions = {
+    rootMargin:"-60px 0px 0px 0px"
+};
+
+const sectionOneObserver = new IntersectionObserver(function (
+  entries,
+  sectionOneObserver
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      nav.classList.remove("start");
+      //Brandname.classList.remove("start");//
+      logo.classList.remove("start");
+      toggle.classList.remove("start");
+      navbarLinks.classList.remove("start");
+    } else {
+      nav.classList.add("start");
+      logo.classList.add("start");
+      //Brandname.classList.add("start");//
+      toggle.classList.add("start");
+      navbarLinks.classList.add("start");
+    }
+  });
+},
+sectionOneOptions);
+
+sectionOneObserver.observe(sectionOne);
+
+let menuOpen = false;
+
+toggleButton.addEventListener("click", () => {
+  toggleButton.classList.toggle("open");
+  navbarLinks.classList.toggle("active");
+});
